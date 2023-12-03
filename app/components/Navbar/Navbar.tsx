27bf779b63d39087deb28505f56c9c3ca572b141
hoainam2001 <78912593+hoainam2001@users.noreply.github.com>
2023-12-03 14:37:05 +0700

    fix: responsive navbar and dark mode

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -5,7 +5,10 @@ import {
   Navbar,
   NavbarBrand,
   NavbarContent,
-  NavbarItem
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle
 } from '@nextui-org/react';
 import { Image } from '@nextui-org/react';
 
@@ -15,6 +18,9 @@ import { useEffect, useState } from 'react';
 const NavbarCom = () => {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = ['Home', 'About', 'Contact'];
 
   useEffect(() => {
     setMounted(true);
@@ -23,8 +29,9 @@ const NavbarCom = () => {
   if (!mounted) return null;
 
   return (
-    <Navbar>
+    <Navbar onMenuOpenChange={setIsMenuOpen} isBordered isMenuOpen={isMenuOpen}>
       <NavbarBrand>
+        <NavbarMenuToggle className="sm:hidden mr-2" />
         <p className="font-bold text-inherit">Profilo</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="end">
@@ -79,6 +86,52 @@ const NavbarCom = () => {
           )}
         </NavbarItem>
       </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2
+                  ? 'primary'
+                  : index === menuItems.length - 1
+                  ? 'danger'
+                  : 'foreground'
+              }
+              className="w-full"
+              href={`/${item === 'Home' ? '' : item.toLowerCase()}`}
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+        <NavbarMenuItem>
+          {theme === 'dark' ? (
+            <div
+              className="bg-white cursor-pointer p-1 rounded-full w-7"
+              onClick={() => setTheme('light')}
+            >
+              <Image
+                width={'25px'}
+                height={'25px'}
+                src="/dark.png"
+                alt="Dark mode"
+              />
+            </div>
+          ) : (
+            <div
+              className="w-7 cursor-pointer p-1 rounded-full bg-[#ccc]"
+              onClick={() => setTheme('dark')}
+            >
+              <Image
+                width={'25px'}
+                height={'25px'}
+                src="/light.png"
+                alt="Light mode"
+              />
+            </div>
+          )}
+        </NavbarMenuItem>
+      </NavbarMenu>
     </Navbar>
   );
 };
